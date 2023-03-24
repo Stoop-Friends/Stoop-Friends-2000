5715d3d2eb52718f24bbe75883a88162cebe39bc
Naomi Brender <NaomiBBrender@gmail.com>
2023-03-24 12:18:18 -0400

    fingers crossed

---
+++ App.js
@@ -0,0 +1,68 @@
+// import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+} from 'react-native';
+import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
+import Map from './components/Map';
+// import Post from './components/Post';
+// import {NavigationContainer} from '@react-navigation/native';
+
+// const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style='auto' />
+    // </View>
+
+    // <NavigationContainer>
+    <View style={{flex: 1}}>
+      <Text
+        style={{
+          flex: 1,
+          marginTop: 50,
+          textAlign: 'center',
+          fontSize: 25,
+        }}>
+        Welcome to Super-Stooper
+      </Text>
+
+      {/* <StatusBar style="auto" /> */}
+
+      <Map />
+      <View style={styles.button}>
+        <Button
+          // style={styles.fixToText}
+
+          title="POST A STOOP SALE"
+          color="purple"
+          onPress={() => Alert.alert('look at you, you pressed the button')}
+        />
+      </View>
+    </View>
+    // </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    textAlign: 'center',
+    marginBottom: 30,
+    marginHorizontal: 30,
+    backgroundColor: '#f194ff',
+    borderRadius: 10,
+  },
+});
---
+++ components/Sales.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, Text, View} from 'react-native';
+
+const dummySales = [
+  {
+    id: 1,
+    coordinates: {
+      latitude: 40.75901,
+      longitude: -73.984474,
+    },
+    where: 'Times Square',
+    hours: {
+      start: '9AM',
+      end: '4PM',
+    },
+    stuff: ['cloths', 'books'],
+  },
+  {
+    id: 2,
+    coordinates: {
+      latitude: 40.782864,
+      longitude: -73.965355,
+    },
+    where: 'Central Park',
+    hours: {
+      start: '9AM',
+      end: '4PM',
+    },
+    stuff: ['cloths', 'books', 'media'],
+  },
+];
+
+export default function Sales() {
+  return dummySales.map(sale => {
+    return (
+      <Marker key={sale.id} coordinate={sale.coordinates} pinColor="black">
+        <Callout>
+          <Text>STOOP SALE</Text>
+          <Text>Where: {sale.where}</Text>
+          <Text>
+            Hours: {sale.hours.start} - {sale.hours.end}
+          </Text>
+          <Text>Stuff: {sale.stuff.join(', ')}</Text>
+        </Callout>
+      </Marker>
+    );
+  });
+}
