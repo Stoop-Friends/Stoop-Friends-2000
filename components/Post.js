e83e4dd6094bb5db6ab2c6e3272054c6bde98b62
Jackie Manginelli <jmanginelli11@gmail.com>
2023-04-26 11:51:02 -0400

    added realm stuff

--- components/Post.js
+++ components/Post.js
@@ -1,30 +1,52 @@
 import React, {useState} from 'react';
 import {View, TextInput, StyleSheet, Button, Alert} from 'react-native';
+import Realm from 'realm';
+import {createRealmContext} from '@realm/react';
+
+class UserPost extends Realm.Object {
+  static schema = {
+    name: 'Post',
+    properties: {
+      _id: 'objectId',
+      name: 'string',
+    },
+    primaryKey: '_id',
+  };
+}
+
+const realmConfig = {
+  schema: [UserPost],
+};
+
+const {RealmProvider, useRealm, useObject, useQuery} =
+  createRealmContext(realmConfig);
 
 const Post = () => {
   const [text, onChangeText] = useState('Example');
   // const [number, onChangeNumber] = useState('');
 
   return (
-    <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-
-      <View style={styles.button}>
-        <Button
-          // style={styles.fixToText}
-
-          title="POST A STOOP SALE"
-          color="purple"
-          onPress={() => {
-            return Alert.alert('woo wee, you pressed the button');
-          }}
+    <RealmProvider>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
         />
+
+        <View style={styles.button}>
+          <Button
+            // style={styles.fixToText}
+
+            title="POST A STOOP SALE"
+            color="purple"
+            onPress={() => {
+              return Alert.alert('woo wee, you pressed the button');
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </RealmProvider>
   );
 };
 
