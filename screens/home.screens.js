721f0e1dab5dacb6e69de01a8d0b54dd8f725356
Jackie Manginelli <jmanginelli11@gmail.com>
2023-05-01 14:24:37 -0400

    changed to postwrapper

--- screens/home.screens.js
+++ screens/home.screens.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import Map from '../components/Map';
-import Post from '../components/Post';
+import PostWrapper from '../components/Post';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.flex}>
       <Text style={styles.text}>Welcome to Super-Stooper</Text>
       <Map />
-      <Post />
+      <PostWrapper />
     </SafeAreaView>
   );
 }
