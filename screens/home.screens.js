0823e9402b0a2b3daeea4f0fac82bd0233c2fe8d
Naomi Brender <NaomiBBrender@gmail.com>
2023-05-01 16:06:19 -0400

    we have navigation

--- screens/home.screens.js
+++ screens/home.screens.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
 import Map from '../components/Map';
 import PostWrapper from '../components/Post';
 
-export default function Home() {
+export default function Home({navigation}) {
   return (
     <SafeAreaView style={styles.flex}>
       <Text style={styles.text}>Welcome to Super-Stooper</Text>
       <Map />
-      <PostWrapper />
+      {/* <PostWrapper /> */}
+      <Button title="ADD A SALE" onPress={() => navigation.navigate('Post')} />
     </SafeAreaView>
   );
 }
